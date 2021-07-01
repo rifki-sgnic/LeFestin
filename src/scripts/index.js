b55dcaeb24cf2bc397076c0d9d60dee02e486519
Muhammad Rifki <69866073+rifki-sgnic@users.noreply.github.com>
2021-07-01 10:26:34 +0700

    Initial commit

---
+++ src/scripts/index.js
@@ -0,0 +1,4 @@
+import 'regenerator-runtime'; /* for async await transpile */
+import '../styles/main.css';
+import './navigation.js';
+import './catalogue.js';
---
+++ src/scripts/navigation.js
@@ -0,0 +1,37 @@
+const mobileMenu = document.querySelector(".mobile-menu");
+const mainMenu = document.querySelector("nav");
+const menuExit = document.querySelector(".menu-exit");
+const body = document.querySelector("body")
+
+window.onload = () => {
+  body.classList.remove("preload")
+}
+
+mobileMenu.addEventListener("click", () => {
+  mainMenu.classList.add("main-active");
+});
+
+menuExit.addEventListener("click", () => {
+  mainMenu.classList.remove("main-active");
+});
+
+window.addEventListener("click", function(e){   
+  if (document.getElementById("content").contains(e.target)){
+    mainMenu.classList.remove("main-active");
+  }
+});
+
+var myNav = document.getElementById("mynav");
+window.onscroll = function () {
+  "use strict";
+  if (
+    document.body.scrollTop >= 100 ||
+    document.documentElement.scrollTop >= 100
+  ) {
+    myNav.classList.add("nav-colored");
+    myNav.classList.remove("nav-transparent");
+  } else {
+    myNav.classList.add("nav-transparent");
+    myNav.classList.remove("nav-colored");
+  }
+};
